5545d88ceb66f2f37dfade6680cfb64d7da31f53
Akshat <akshat26april@gmail.com>
2023-02-23 15:48:03 +0530

    Developed all Components

---
+++ src/Components/InputAmount.jsx
@@ -0,0 +1,23 @@
+import { Grid, TextField } from "@mui/material";
+import { useContext } from "react";
+import { CurrencyContext } from "../context/CurrencyContext";
+
+const InputAmount = () => {
+  const { firstAmount, setFirstAmount } = useContext(CurrencyContext);
+
+  return (
+    <Grid item xs={12} md>
+      <TextField
+        value={firstAmount}
+        onChange={(e) => setFirstAmount(e.target.value)}
+        label="Amount"
+        fullWidth
+        InputProps={{
+          type: "number",
+        }}
+      />
+    </Grid>
+  );
+};
+
+export default InputAmount;
---
+++ src/Context/CurrencyContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+export const CurrencyContext = createContext();
+
+const CurrencyProvider = ({ children }) => {
+  const [fromCurrency, setFromCurrency] = useState("🇩🇪 EUR - Germany");
+  const [toCurrency, setToCurrency] = useState("🇮🇳 INR - India");
+  const [firstAmount, setFirstAmount] = useState("");
+
+  const value = {
+    fromCurrency,
+    setFromCurrency,
+    toCurrency,
+    setToCurrency,
+    firstAmount,
+    setFirstAmount
+  };
+  return (
+    <CurrencyContext.Provider value={value}>
+      {children}
+    </CurrencyContext.Provider>
+  );
+};
+
+export default CurrencyProvider;
